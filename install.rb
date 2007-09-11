c4ed8c5d852436aba6474e3d15876cb793c1e6df
meekish <meekish@64ae8ec5-7f57-401c-b70e-9f92ded5742b>
2007-09-11 23:53:41 +0000

    typo
    git-svn-id: http://swxruby.rubyforge.org/svn/trunk@17 64ae8ec5-7f57-401c-b70e-9f92ded5742b

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 # Taken from Railties
 def gsub_file(relative_destination, regexp, *args, &block)
-  path = destination_path(relative_destination)
+  path = relative_destination
   content = File.read(path).gsub(regexp, *args, &block)
   File.open(path, 'wb') { |file| file.write(content) }
 end
