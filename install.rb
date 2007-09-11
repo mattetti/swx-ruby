0d913ba06304a81da473996eac057b152c25e387
meekish <meekish@64ae8ec5-7f57-401c-b70e-9f92ded5742b>
2007-09-11 23:51:36 +0000

    * install.rb file now copies test_data_types.rb to services folder * install.rb now add rSWX gateway route to routes.rb
    git-svn-id: http://swxruby.rubyforge.org/svn/trunk@16 64ae8ec5-7f57-401c-b70e-9f92ded5742b

--- install.rb
+++ install.rb
@@ -1,3 +1,10 @@
+# Taken from Railties
+def gsub_file(relative_destination, regexp, *args, &block)
+  path = destination_path(relative_destination)
+  content = File.read(path).gsub(regexp, *args, &block)
+  File.open(path, 'wb') { |file| file.write(content) }
+end
+
 begin
   require 'fileutils'
 	include FileUtils
@@ -20,6 +27,19 @@ begin
 		mkdir('./app/services')
 	end
 	
+	# Copy TestDataTypes to app/services
+	unless File.exist?('./app/services/test_data_types.rb')
+		puts '*** Copying TestDataTypes service to app/services ***'
+		cp('./vendor/plugins/rswx/lib/services/test_data_types.rb','./app/services/test_data_types.rb')
+	end
+	
+	# Add route for rSWX gateway to routes.rb
+	sentinel = 'ActionController::Routing::Routes.draw do |map|'
+	gsub_file './config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+	  "#{match}\n  map.swx '/swx', :controller => 'swx', :action => 'gateway'\n"
+	end
+
+
 rescue Exception => e
   puts 'ERROR INSTALLING rSWX: ' + e.message
 end
