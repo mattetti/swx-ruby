7a18397a24b4cb8e81b8c8d31134b1017ce14d2b
meekish <meekish@64ae8ec5-7f57-401c-b70e-9f92ded5742b>
2007-10-08 19:49:16 +0000

    * Install script now prints out instructions from README. * Refactored copy commands to no longer rely on a specific dir name for the plugin directory (i.e. 'swx_on_rails')
    git-svn-id: http://swxruby.rubyforge.org/svn/trunk@56 64ae8ec5-7f57-401c-b70e-9f92ded5742b

--- install.rb
+++ install.rb
@@ -5,67 +5,8 @@ def gsub_file(relative_destination, regexp, *args, &block)
   File.open(path, 'wb') { |file| file.write(content) }
 end
 
-@what_now = <<-INSTRUCTIONS
-
-=======================================
-SWX Ruby: SWX on Rails Plugin Alpha 0.1
-=======================================
-
-GETTING STARTED
-===============
-SWX on Rails will look for your service classes in RAILS_ROOT/app/services.
-Simply create standard Ruby classes and drop them in this folder. Service classes 
-are composed of instance methods. SWX on Rails will instantiate your service
-class, call the specified method, and send the response back to the Flash Player.
-
-Take a peek at app/services/hello_world.rb for a working service class example.
-Alright, alright, I'll just show it to you here:
-
-# hello_world.rb----------------------------------------
-# Class and method names follow standard Ruby convention
-class HelloWorld
-	# Service class methods are instance methods.
-	def just_say_the_words
-		'Hello World!'
-	end
-end
-#-------------------------------------------------------
-
-Here's a Moo card-esque example to call HelloWorld#just_say_the_words from the 
-Flash Player (place a MovieClip on stage with an instance name of 'loader' and
-fire up your development server):
-
-//------------------------------------------------------
-loader.serviceClass = "HelloWorld";
-// Method names follow ActionScript convention 
-// (converted to underscored server-side)
-loader.method = "justSayTheWords";
-loader.debug = true;
-loader.loadMovie("http://localhost:3000/swx", "POST");
-
-function onEnterFrame() {
- // Will output 'Hello World!' once the SWX file is loaded.
- trace(loader.result);
-}
-//------------------------------------------------------
-
-When you're ready for some robust ActionScript trickery, head to 
-http://swxformat.org/download/ to grab the SWX ActionScript library.
-
-Oh yeah, you may return ActiveRecord objects from your service classes; 
-SWX on Rails will happily serialize them for you. Go ahead, give it a try!
-
-SWX on Rails is very alpha and may break, throw its toys, eat your
-firstborn child, etc.; it is sure to be full of gaping security holes. DO NOT
-open an Alpha 0.1 gateway to untrusted data! Please post bug reports/suggestions to 
-http://groups.google.com/group/swxruby. Full-featured tracker coming soon.
-=======================================
-NOTE: You may notice some Security Sandbox Violations when testing the example
-above in the Flash IDE. Rest assured, this is OK. Visit 
-http://swxformat.org/132 for further explanation.
-
-
-INSTRUCTIONS
+SWX_RUBY_ROOT = File.dirname(__FILE__)
+@getting_started = IO.read(File.join(SWX_RUBY_ROOT, 'README'))
 
 begin
   require 'fileutils'
@@ -74,13 +15,13 @@ begin
 	# Copy config file
   unless File.exist?('./config/swx.yml')
 		puts '*** Copying config file to config/swx.yml ***'
-    cp('./vendor/plugins/swx_on_rails/lib/config/swx.yml', './config/swx.yml')
+    cp(File.join(SWX_RUBY_ROOT, 'lib', 'config', 'swx.yml'), './config/swx.yml')
   end
   
 	# Copy SWX controller
 	unless File.exist?('./app/controllers/swx_controller.rb')
 		puts '*** Copying SWX controller to app/controllers/swx_controller.rb ***'
-	  cp('./vendor/plugins/swx_on_rails/lib/controllers/swx_controller.rb','./app/controllers/swx_controller.rb')
+	  cp(File.join(SWX_RUBY_ROOT, 'lib', 'controllers', 'swx_controller.rb'), './app/controllers/swx_controller.rb')
 	end
 	
 	# Create services directory
@@ -92,13 +33,13 @@ begin
 	# Copy TestDataTypes class to app/services
 	unless File.exist?('./app/services/test_data_types.rb')
 		puts '*** Copying TestDataTypes service class to app/services ***'
-		cp('./vendor/plugins/swx_on_rails/lib/services/test_data_types.rb','./app/services/test_data_types.rb')
+		cp(File.join(SWX_RUBY_ROOT, 'lib', 'services', 'test_data_types.rb'), './app/services/test_data_types.rb')
 	end
 	
 	# Copy HelloWorld class to app/services
 	unless File.exist?('./app/services/hello_world.rb')
 		puts '*** Copying HelloWorld service class to app/services ***'
-		cp('./vendor/plugins/swx_on_rails/lib/services/hello_world.rb','./app/services/hello_world.rb')
+		cp(File.join(SWX_RUBY_ROOT, 'lib', 'services', 'hello_world.rb'), './app/services/hello_world.rb')
 	end
 	
 	# Add route for SWX gateway to routes.rb
@@ -114,12 +55,12 @@ begin
 	require 'json'
 	puts ': JSON gem detected ***'
 	
-	puts @what_now
+	puts @getting_started
 rescue LoadError
-	puts @what_now
+	puts @getting_started
 	
 	puts '!!!!! You do not have the JSON gem installed. SWX on Rails will not function without it.'
 	puts '!!!!! Please "gem install json" to get the JSON gem, then SWX on Rails should be ready to roll.'
 rescue Exception => e
-  puts 'ERROR INSTALLING rSWX: ' + e.message
+  puts 'ERROR INSTALLING SWX Ruby: ' + e.message
 end
