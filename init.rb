cf91b5967aa70c30774aadd76fb4a79742e20b38
meekish <meekish@64ae8ec5-7f57-401c-b70e-9f92ded5742b>
2007-10-16 07:07:52 +0000

    Rails controller integration complete. 'render :swx => @my_funky_data' to your heart's content!
    git-svn-id: http://swxruby.rubyforge.org/svn/trunk@61 64ae8ec5-7f57-401c-b70e-9f92ded5742b

--- init.rb
+++ init.rb
@@ -4,3 +4,9 @@ require 'yaml'
 # Tell SwxGateway where app root and config file are located
 SwxGateway.app_root = RAILS_ROOT
 SwxGateway.swx_config = YAML.load_file(File.join(RAILS_ROOT, 'config', 'swx.yml'))
+
+# Initialize Rails integration
+require File.expand_path(File.join(File.dirname(__FILE__), 'lib', 'rails_integration', 'render_decorator'))
+
+# Register the SWX mime tytpe
+Mime::Type.register "application/swf", :swx
